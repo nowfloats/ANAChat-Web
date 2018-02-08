c8f4004362f22aff00141ad68cdf557adfba760c
Khaja Nizamuddin <khajanizamuddin@hotmail.com>
2018-02-09 03:04:45 +0530

    loading added

--- src/app/components/chat-thread/chat-thread.component.ts
+++ src/app/components/chat-thread/chat-thread.component.ts
@@ -376,7 +376,16 @@ export class ChatThreadComponent implements OnInit, AfterViewInit {
 				this.removeConsecutiveErrorsMessage();
 				this.chatThread.addTextIncoming(Config.consecutiveErrorsMessageText, Config.consecutiveErrorsMessageAckId);
 			};
-			this.loadHistory(() => this.stompService.connect());
+			this.loadHistory(() => {
+				try {
+					if (window.parent && window.parent.postMessage) {
+						window.parent.postMessage({
+							type: "LOADED"
+						}, "*");
+					}
+				} catch (e) { }
+				this.stompService.connect()
+			});
 		}
 
 		if (this.settings.simulatorMode) {
