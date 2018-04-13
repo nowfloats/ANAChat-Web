d237ead87e994ec73f3eb576a99f8571cdb73145
Khaja Nizamuddin <khajanizamuddin@hotmail.com>
2018-04-13 17:58:22 +0530

    fixed loading indicator in non fullpage mode

--- src/assets/embed/ana-web-chat-plugin.js
+++ src/assets/embed/ana-web-chat-plugin.js
@@ -621,13 +621,13 @@
 	left: 0;
 	bottom: 0;
 	right: 0;
-	position: fixed;
+	position: absolute;
 	background-color: rgba(0,0,0,0.1);
 	border-radius: 10px;
 }
 
 .ana-loading-overlay .ana-loading {
-	position: fixed;
+	position: absolute;
 	top: 50%;
 	left: 50%;
 	transform: translate(-50%,-50%);
