3323377fb8846782b16b5ec2962e5347b2995d38
Khaja Nizamuddin <khajanizamuddin@hotmail.com>
2018-04-18 05:19:29 +0530

    handled js cookie errors when cookies are not available.

--- src/assets/embed/ana-web-chat-plugin.js
+++ src/assets/embed/ana-web-chat-plugin.js
@@ -15,14 +15,21 @@
 			};
 		},
 		setCookie: (name, value, days = 7, path = '/') => {
-			const expires = new Date(Date.now() + days * 864e5).toUTCString()
-			document.cookie = name + '=' + encodeURIComponent(value) + '; expires=' + expires + '; path=' + path
+			try {
+				const expires = new Date(Date.now() + days * 864e5).toUTCString()
+				document.cookie = name + '=' + encodeURIComponent(value) + '; expires=' + expires + '; path=' + path
+			} catch (e) {
+			}
 		},
 		getCookie: (name) => {
-			return document.cookie.split('; ').reduce((r, v) => {
-				const parts = v.split('=')
-				return parts[0] === name ? decodeURIComponent(parts[1]) : r
-			}, '')
+			try {
+				return document.cookie.split('; ').reduce((r, v) => {
+					const parts = v.split('=')
+					return parts[0] === name ? decodeURIComponent(parts[1]) : r
+				}, '')
+			} catch (e) {
+				return '';
+			}
 		},
 		deleteCookie: (name, path) => {
 			setCookie(name, '', -1, path)
